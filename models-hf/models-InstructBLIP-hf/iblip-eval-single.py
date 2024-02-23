02eac1295b35aae51f8b943d61069836631243a8
RAHUL MEHTA <rahcode7@users.noreply.github.com>
2024-02-23 18:18:12 +0530

    Update iblip-eval-single.py

--- models-hf/models-InstructBLIP-hf/iblip-eval-single.py
+++ models-hf/models-InstructBLIP-hf/iblip-eval-single.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
                 else:
                     prompt += q.split("?")[0] + " ? "
                 
-                inputs = processor(images=image, text=prompt, return_tensors="pt").to(device,dtype=torch.bfloat16)
+                inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
 
                 outputs = model.generate(
                     **inputs,
